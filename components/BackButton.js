a9ad1c918f04c3d68e06025371d402d2ed196c2b
Ruaidhri Lumsden <ruaidhri@lumsden.digital>
2023-01-18 21:43:25 +0000

    Added hover effect to navbar buttons and backbutton

--- components/BackButton.js
+++ components/BackButton.js
@@ -3,6 +3,6 @@ import Link from 'next/link'
 
 export default function BackButton(props) {
   return (
-    <Link href="/"><div className={`bg-slate-100 w-max px-2 py-1 rounded-sm cursor-pointer ${props.ml && 'ml-5'} ${props.top && 'mb-4'} ${props.bottom && 'mt-4'} ${props.mt && 'mt-10'}`}>back</div></Link>
+    <Link href="/"><div className={`bg-slate-100 w-max px-2 py-1 rounded-sm cursor-pointer ${props.ml && 'ml-5'} ${props.top && 'mb-4'} ${props.bottom && 'mt-4'} ${props.mt && 'mt-10'} hover:bg-slate-300 transition-colors duration-300`}>back</div></Link>
   )
 }
